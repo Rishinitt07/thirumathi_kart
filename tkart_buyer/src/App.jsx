fbe81c60c69dcf7f4cac0c535a3c1d5240989778
dhanas3kar <dhanasekarmurgesan@gmail.com>
2025-06-13 11:18:50 +0530

    updated

--- tkart_buyer/src/App.jsx
+++ tkart_buyer/src/App.jsx
@@ -3,10 +3,10 @@ import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
-import Categories from './components/categories';
-import Profile from './components/Profile'; 
-import Wishlist from './components/wishlist';
-import MyCart from './components/MyCart'
+import Categories from './components/Sub-Components/categories';
+import Profile from './components/Sub-Components/Profile'; 
+import Wishlist from './components/Sub-Components/wishlist';
+import MyCart from './components/Sub-Components/MyCart'
 
 function App() {
   return (
@@ -14,12 +14,13 @@ function App() {
       <Routes>
         <Route path='/' element={<Dashboard />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/categories' element={<Categories />} />
         <Route path='/register' element={<Register />} />
         <Route path='/Home' element={<Home />} />
+         <Route path='/categories' element={<Categories />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/Wishlist" element={<Wishlist />} />
          <Route path="/MyCart" element={<MyCart />} />
+         
       </Routes>
     </div>
   );
